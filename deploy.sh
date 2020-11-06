4128b58f876b58f6ae29d489ce6325a1bdd4d80c
dasha <hrebtovadara@gmail.com>
2020-11-06 16:51:54 +0300

    add github io

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:hrebtovadara/catalog-products.git master:gh-pages
+git push -f git@github.com:hrebtovadara/my-test-catalog.git master:gh-pages
 
 cd -
