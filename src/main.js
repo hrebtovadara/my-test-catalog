3dc94105eeb671a9f64c207057d9bdc364e62a58
dasha <hrebtovadara@gmail.com>
2020-11-06 11:24:31 +0300

    add github io

--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.js = module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/catalog-products/'
-      : '/'
-}
-
-
+// Vue.config.productionTip = false
 
 new Vue({
   router,
