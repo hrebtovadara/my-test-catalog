0b98ad736ee8b420cf81b5487f5d859621f62cd3
dasha <hrebtovadara@gmail.com>
2020-11-06 12:54:07 +0300

    add github io

--- src/deploy.sh
+++ src/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hrebtovadara/catalog-products.git master:gh-pages
+git push -f git@github.com:<hrebtovadara>/<catalog-products>.git master:gh-pages
 
 cd -
